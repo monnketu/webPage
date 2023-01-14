a0dc5dd5916d70ee2f1a9fe2609ea5d61ff83d30
NoguchiHiroto <Hirotodrill0831@gmail.com>
2023-01-14 15:25:50 +0900

    connect and create dababase

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import test
+from .models import coWorkingSpace
 
 # Register your models here.
+admin.site.register(test)
+admin.site.register(coWorkingSpace)
