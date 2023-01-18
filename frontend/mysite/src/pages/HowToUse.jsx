b738d258ae23189e1cb6fa3fa199408ca8571824
藤田陶子 <fujitatoko@13351066037.ap.cc.tsukuba.ac.jp>
2023-01-18 16:13:35 +0900

    mod how-to-use

---
+++ frontend/mysite/src/pages/HowToUse.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Title from '../components/howToUse/title';
+
+const HowToUse = () => {
+  return (
+    <div>
+      <Title title={'特徴と使い方'}/>
+      <Title title={'フリースタイルとは'}/>
+      <Title title={'さあ、使ってみましょう'}/>
+    </div>
+  )
+}
+
+export default HowToUse
