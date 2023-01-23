98181865306b55253eae764f7605f0d86bd9ad51
藤田陶子 <fujitatoko@fujitatoukonoMacBook-Pro.local>
2023-01-23 16:02:11 +0900

    create-params

--- backend/polls/urls.py
+++ backend/polls/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
+    path('coupon/', views.coupon, name='coupon'),
     path('templates/', views.index_template, name='index_template'),
     path('templates/test1/', views.index_template_test1, name='index_template_test1'),
 ]
--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import coWorkingSpace
+from .models import coWorkingSpace # Couponクラスをインポート
+import json
+
+
+def coupon(request):
+    data = coWorkingSpace.objects.all() # テーブルにある全てのレコードを取得
+
+    l = {}
+
+    for i in range(18):
+        params = {
+            # 'coupon_code':str(data[0].code), # 個々のレコードはdata型なのでString型にキャストする
+            # 'coupon_benefits':str(data[0].benefit),
+            # 'coupon_explanation':str(data[0].explanation),
+            # 'coupon_store':str(data[0].store),
+            # 'coupon_start':str(data[0].start),
+            # 'coupon_deadline':str(data[0].deadline),
+            'coupon_status':str(data[i].name),
+            }
+        l['param' + str(i)] = json.dumps(params, ensure_ascii=False, indent=2)
+
+    
+        
+
+
+    json_str = json.dumps(l, ensure_ascii=False, indent=2)
+    return HttpResponse(json_str)
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
