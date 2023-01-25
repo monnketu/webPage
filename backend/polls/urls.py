b8bab687f1100572d487358c7005c740babb3be8
藤田陶子 <fujitatoko@fujitatoukonoMacBook-Pro.local>
2023-01-25 18:44:09 +0900

    change-views.py

--- backend/polls/urls.py
+++ backend/polls/urls.py
@@ -10,4 +10,13 @@ urlpatterns = [
     path('line_api/', views.getLineAccessToken, name='getLineAccessToken'),
     path('templates/', views.index_template, name='index_template'),
     path('templates/test1/', views.index_template_test1, name='index_template_test1'),
+    path('coupon/shibuya', views.coupon, name='coupon'),
+    path('coupon/shinjuku', views.coupon, name='coupon'),
+    path('coupon/ikebukuro', views.coupon, name='coupon'),
+    path('coupon/tokyo', views.coupon, name='coupon'),
+    path('coupon/shinagawa', views.coupon, name='coupon'),
+    path('coupon/others', views.coupon, name='coupon'),
+
+
+
 ]
--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import coWorkingSpace # Couponクラスをインポート
-import requests
 import json
+import math
 
 
-def coupon(request):
+def coupon_shibuya(request):
 
   # 以下のdata変数をすべてのデータを取得から与えられた条件に一致するものを検索して取得に変更
-  data = coWorkingSpace.objects.all() # テーブルにある全てのレコードを取得
+  # data = coWorkingSpace.objects.all() # テーブルにある全てのレコードを取得
+  data = coWorkingSpace.objects.filter(aria = "渋谷")
+
+  print(data)
   l = {}
 
-  for i in range(17):
+  for i in range(len(data)):
     params = {      
       'coupon_spaceID':str(data[i].spaceID),
       'coupon_aria':str(data[i].aria),
@@ -30,26 +33,157 @@ def coupon(request):
     }
     l['param' + str(i)] = json.dumps(params, ensure_ascii=False, indent=2)
 
+def coupon_shinjuku(request):
+
+  # 以下のdata変数をすべてのデータを取得から与えられた条件に一致するものを検索して取得に変更
+  # data = coWorkingSpace.objects.all() # テーブルにある全てのレコードを取得
+  data = coWorkingSpace.objects.filter(aria = "新宿")
+
+  print(data)
+  l = {}
+
+  for i in range(len(data)):
+    params = {      
+      'coupon_spaceID':str(data[i].spaceID),
+      'coupon_aria':str(data[i].aria),
+      'coupon_name':str(data[i].name),
+      'coupon_address':str(data[i].address),
+      'coupon_station':str(data[i].station),
+      'coupon_wi-fi':str(data[i].wifi),
+      'coupon_isBattery':str(data[i].isBattery),
+      'coupon_isMeetingRoom':str(data[i].isMeetingRoom),
+      'coupon_isDropIn':str(data[i].isDropIn),
+      'coupon_price':str(data[i].price),
+      'coupon_startTime':str(data[i].startTime),
+      'coupon_endTime':str(data[i].endTime),
+      'coupon_favorite':str(data[i].favorite),
+      'coupon_jurisdiction':str(data[i].jurisdiction),
+    }
+    l['param' + str(i)] = json.dumps(params, ensure_ascii=False, indent=2)
+
+
+def coupon_ikebukuro(request):
+
+  # 以下のdata変数をすべてのデータを取得から与えられた条件に一致するものを検索して取得に変更
+  # data = coWorkingSpace.objects.all() # テーブルにある全てのレコードを取得
+  data = coWorkingSpace.objects.filter(aria = "池袋")
+
+  print(data)
+  l = {}
+
+  for i in range(len(data)):
+    params = {      
+      'coupon_spaceID':str(data[i].spaceID),
+      'coupon_aria':str(data[i].aria),
+      'coupon_name':str(data[i].name),
+      'coupon_address':str(data[i].address),
+      'coupon_station':str(data[i].station),
+      'coupon_wi-fi':str(data[i].wifi),
+      'coupon_isBattery':str(data[i].isBattery),
+      'coupon_isMeetingRoom':str(data[i].isMeetingRoom),
+      'coupon_isDropIn':str(data[i].isDropIn),
+      'coupon_price':str(data[i].price),
+      'coupon_startTime':str(data[i].startTime),
+      'coupon_endTime':str(data[i].endTime),
+      'coupon_favorite':str(data[i].favorite),
+      'coupon_jurisdiction':str(data[i].jurisdiction),
+    }
+    l['param' + str(i)] = json.dumps(params, ensure_ascii=False, indent=2)
+
+def coupon_tokyo(request):
+
+  # 以下のdata変数をすべてのデータを取得から与えられた条件に一致するものを検索して取得に変更
+  # data = coWorkingSpace.objects.all() # テーブルにある全てのレコードを取得
+  data = coWorkingSpace.objects.filter(aria = "東京")
+
+  print(data)
+  l = {}
+
+  for i in range(len(data)):
+    params = {      
+      'coupon_spaceID':str(data[i].spaceID),
+      'coupon_aria':str(data[i].aria),
+      'coupon_name':str(data[i].name),
+      'coupon_address':str(data[i].address),
+      'coupon_station':str(data[i].station),
+      'coupon_wi-fi':str(data[i].wifi),
+      'coupon_isBattery':str(data[i].isBattery),
+      'coupon_isMeetingRoom':str(data[i].isMeetingRoom),
+      'coupon_isDropIn':str(data[i].isDropIn),
+      'coupon_price':str(data[i].price),
+      'coupon_startTime':str(data[i].startTime),
+      'coupon_endTime':str(data[i].endTime),
+      'coupon_favorite':str(data[i].favorite),
+      'coupon_jurisdiction':str(data[i].jurisdiction),
+    }
+    l['param' + str(i)] = json.dumps(params, ensure_ascii=False, indent=2)
+
+
+def coupon_shinagawa(request):
+
+  # 以下のdata変数をすべてのデータを取得から与えられた条件に一致するものを検索して取得に変更
+  # data = coWorkingSpace.objects.all() # テーブルにある全てのレコードを取得
+  data = coWorkingSpace.objects.filter(aria = "品川")
+
+  print(data)
+  l = {}
+
+  for i in range(len(data)):
+    params = {      
+      'coupon_spaceID':str(data[i].spaceID),
+      'coupon_aria':str(data[i].aria),
+      'coupon_name':str(data[i].name),
+      'coupon_address':str(data[i].address),
+      'coupon_station':str(data[i].station),
+      'coupon_wi-fi':str(data[i].wifi),
+      'coupon_isBattery':str(data[i].isBattery),
+      'coupon_isMeetingRoom':str(data[i].isMeetingRoom),
+      'coupon_isDropIn':str(data[i].isDropIn),
+      'coupon_price':str(data[i].price),
+      'coupon_startTime':str(data[i].startTime),
+      'coupon_endTime':str(data[i].endTime),
+      'coupon_favorite':str(data[i].favorite),
+      'coupon_jurisdiction':str(data[i].jurisdiction),
+    }
+    l['param' + str(i)] = json.dumps(params, ensure_ascii=False, indent=2)
   
-      
+def coupon_others(request):
+
+  # 以下のdata変数をすべてのデータを取得から与えられた条件に一致するものを検索して取得に変更
+  # data = coWorkingSpace.objects.all() # テーブルにある全てのレコードを取得
+  data = coWorkingSpace.objects.filter(aria = "その他")
+
+  print(data)
+  l = {}
+
+  for i in range(len(data)):
+    params = {      
+      'coupon_spaceID':str(data[i].spaceID),
+      'coupon_aria':str(data[i].aria),
+      'coupon_name':str(data[i].name),
+      'coupon_address':str(data[i].address),
+      'coupon_station':str(data[i].station),
+      'coupon_wi-fi':str(data[i].wifi),
+      'coupon_isBattery':str(data[i].isBattery),
+      'coupon_isMeetingRoom':str(data[i].isMeetingRoom),
+      'coupon_isDropIn':str(data[i].isDropIn),
+      'coupon_price':str(data[i].price),
+      'coupon_startTime':str(data[i].startTime),
+      'coupon_endTime':str(data[i].endTime),
+      'coupon_favorite':str(data[i].favorite),
+      'coupon_jurisdiction':str(data[i].jurisdiction),
+    }
+    l['param' + str(i)] = json.dumps(params, ensure_ascii=False, indent=2)
+       
 
 
   json_str = json.dumps(l, ensure_ascii=False, indent=2)
   return HttpResponse(json_str)
 
-def getLineAccessToken(request):
-  print(request)
-  if (request.method == 'POST'): 
-    print('POST')
-  return HttpResponse(request)
+
 
 def index(request):
   return HttpResponse("Hello, world. You're at the polls index.")
-
-def getSimData(request):
-  # とーこへ　ここの関数の中に、データをとってくる処理、類似度を測定する処理を書いて欲しい
-  return HttpResponse("Hello, world. You're at the polls getSimData.")
-
 def index_template(request):
   print(request)
   ctx = {}
@@ -65,3 +199,24 @@ def search_result(request):
   print(request)
   return render(request, 'search_result.html', ctx)
 # Create your views here.
+# 
+
+def sim_jaccard(prefs, a, b):
+    si = {}
+    for item in prefs[a]:
+        if item in prefs[b]:
+            si[item] = 1
+
+    n = len(si)
+    if n == 0:
+        return 0
+
+    len_a = len(prefs[a])
+    len_b = len(prefs[b])
+
+    return n / (len_a + len_b - n)
+
+def getSimData(request):
+  
+  # とーこへ　ここの関数の中に、データをとってくる処理、類似度を測定する処理を書いて欲しい
+  return HttpResponse("Hello, world. You're at the polls getSimData.")
