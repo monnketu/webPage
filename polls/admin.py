f68433e767fb44ae57d203dcf14145c6f5f910f0
Noguchi-Hiroto <hiroto.noguchi@emerada.co.jp>
2023-01-26 18:42:12 +0900

    delete folders

--- polls/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import test
-from .models import coWorkingSpace
-
-# Register your models here.
-admin.site.register(test)
-admin.site.register(coWorkingSpace)
