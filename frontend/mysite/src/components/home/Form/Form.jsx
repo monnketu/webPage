558f825c89038ee9d0b34d3ff97c0486f7e15d4a
Noguchi-Hiroto <hiroto.noguchi@emerada.co.jp>
2023-01-27 13:38:40 +0900

    search

--- frontend/mysite/src/components/home/Form/Form.jsx
+++ frontend/mysite/src/components/home/Form/Form.jsx
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import styles from './../../../styles/home/Form/form.module.scss';
 
 const Form = (props) => {
+  const navigate = useNavigate();
+  const [space, setSpace] = useState('all');
+  const [dropIn, setDropIn] = useState('all');
+  const [time, setTime] = useState(null);
+  
+
   return (
     <div className= {styles.search_box}>
       <p className = {styles.search_sentence} >場所、日時から探す</p>
-      <label htmlFor="cafe" className={styles.radioButtonLabel}>カフェ</label>
-      <input id='cafe' type='radio' name='cafeORcoWokingSpace'/>
-      <label htmlFor="coWorkingSpace" className={styles.radioButtonLabel}>コワーキングスペース</label>
-      <input id='coWorkingSpace' type='radio' name='cafeORcoWokingSpace'/>
+      <select className = {styles.search_option} name="example" onChange={(e) => setSpace(e.target.value)}>
+        <option value='all'>指定なし</option>
+        <option value='cafe'>カフェ</option>
+        <option value='coWorkingSpace'>コワーキングスペース</option>
+      </select>
       {/* <input class = {styles.search_criteria} type = "text" placeholder="エリア・駅"/>
       <input class = {styles.search_criteria} type = "text" placeholder="キーワード・名前" /> */}
-      <select className = {styles.search_option} name="example">
-        <option value="">選択して下さい</option>
-        <option>月額契約</option>
-        <option>ドロップイン可能</option>
+      <select className = {styles.search_option} name="example" onChange={(e) => setDropIn(e.target.value)}>
+        <option value="all">指定なし</option>
+        <option value='monthly'>月額契約</option>
+        <option value='dropIn'>ドロップイン可能</option>
       </select>
-      <input className = {styles.search_date} type = "date" />
-      <input className = {styles.search_time} type = "time" />
-      <button className = {styles.search_button}>検索</button>
+
+      <input className = {styles.search_time} type = "time" onChange={(e) => setTime(e.target.value)}/>
+      <button className = {styles.search_button} onClick={() => navigate('/search_result', {state:{space: space, dropIn: dropIn, time: time, searchedByForm: true}})}>検索</button>
     </div>
   )
 }
--- frontend/mysite/src/pages/SearchResult.jsx
+++ frontend/mysite/src/pages/SearchResult.jsx
@@ -3,13 +3,17 @@ import { useLocation } from "react-router-dom";
 import Header from '../components/searchResult/header/Header';
 import Results from '../components/searchResult/resultSpace/Results';
 import SideMenu from '../components/searchResult/sideMenu/SideMenu';
-
+import moment from 'moment';
 import styles from './../styles/searchResult/searchResult.module.scss'
 const SearchResult = () => {
   const location = useLocation();
-  const info = location.state ? location.state.info: {ja: '全て', en: 'all'};
+  const isLocationState = location.state ? true : false;
+  const place = isLocationState ? (location.state.space !== 'all' ? location.state.space: '') : '';
+  const dropIn = isLocationState ? (location.state.dropIn !== 'all' ? location.state.dropIn: '') : '';
+  const time = isLocationState ? (location.state.time !== null ? location.state.time: '') : '';
+  const dispName = isLocationState ? ((place===''&&dropIn===''&&time==='') ? '全て':`${place} ${dropIn} ${time}`): '全て' 
+  const info = isLocationState ? (location.state.searchedByForm ? {...location.state, en:'all', ja:dispName} : location.state.info): {ja: '全て', en: 'all'};
   const [ data, setData ] = useState([]);
-  // const ariaName = location.state.aria;
   useEffect(() => {
     try {
       fetch(`http://localhost:8000/api/coWorkingSpace/${info.en}/`)
@@ -17,8 +21,42 @@ const SearchResult = () => {
         return response.json();
       })
       .then(res => {
-        console.log(res)
-        setData(res.results);
+        console.log(res,location.state);
+        let ret = res.results;
+        if (isLocationState && location.state.searchedByForm) {
+          ret = ret.filter((data) => {
+            switch(place) {
+              case 'cafe': {
+                return data.isCafe === true;
+              }
+              case 'coWorkingSpace':{
+                return data.isCafe === false;
+              }
+              default: {
+                return data;
+              }
+            }
+          }).filter((data) => {
+            switch(dropIn) {
+              case 'dropIn': {
+                return data.isDropIn === true;
+              }
+              case 'monthly': {
+                return data.isDropIn === false;
+              }
+              default: {
+                return data;
+              }
+            }
+          })
+          // .filter((data) => {
+          //   const space_start_time = data.startTime === null ? moment('2023-01-20T00:00:00').format('h:mm:ss') : moment(`2023-04-23T${location.state.time}:00`).format('hh:mm:ss');
+          //   const space_end_time = data.endTime === null ? moment(`${new Date()}T23:59:59`).format('h:mm:ss') : moment(data.endTime).format('h:mm:ss');
+          //   console.log(space_start_time, space_end_time, new Date());
+          //   return moment(location.state.time).isBetween(space_start_time, space_end_time);
+          // })
+        }
+        setData(ret);
       })
     } catch (error) {
       console.log("失敗しました");
