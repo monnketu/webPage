35e2a5026a65c124536fe68262793dc4860a3f64
Noguchi-Hiroto <hiroto.noguchi@emerada.co.jp>
2023-02-10 09:01:21 +0900

    診断の選択肢の文字を変更

--- frontend/mysite/src/components/Questionnaire/Answers.tsx
+++ frontend/mysite/src/components/Questionnaire/Answers.tsx
@@ -3,16 +3,18 @@ import Answer from './Answer';
 import styles from './../../styles/Questionnaire/answers.module.scss';
 
 interface Props {
+  left: string;
+  right: string;
   questionNumber: number;
 }
 export default function Answers(props:Props) {
   return (
     <div className={styles.answersContainer}>
-      <Answer text={'価格'} rank={5} questionNumber={props.questionNumber} />
-      <Answer text={'やや価格'} rank={3} questionNumber={props.questionNumber} />
+      <Answer text={props.left} rank={5} questionNumber={props.questionNumber} />
+      <Answer text={'やや' + props.left} rank={3} questionNumber={props.questionNumber} />
       <Answer text={'同程度'} rank={1} questionNumber={props.questionNumber} />
-      <Answer text={'ややアクセス'} rank={1/3} questionNumber={props.questionNumber} />
-      <Answer text={'アクセス'} rank={1/5} questionNumber={props.questionNumber} />
+      <Answer text={'やや' + props.right} rank={1/3} questionNumber={props.questionNumber} />
+      <Answer text={props.right} rank={1/5} questionNumber={props.questionNumber} />
     </div>
   )
 }
--- frontend/mysite/src/pages/Questionnaire.tsx
+++ frontend/mysite/src/pages/Questionnaire.tsx
@@ -6,11 +6,11 @@ export default function Questionnaire() {
   return (
     <div className={styles.QuestionnaireContainer}>
       <h2 className={styles.question}>←価格&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;アクセス→</h2>
-      <Answers questionNumber={1} />
-      <h2 className={styles.question}>Q2 場所</h2>
-      <Answers questionNumber={2}/>
-      <h2 className={styles.question}>Q3 ドロップイン</h2>
-      <Answers questionNumber={3}/>
+      <Answers questionNumber={1} left='価格' right='アクセス' />
+      <h2 className={styles.question}>←アクセス&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;営業形態→</h2>
+      <Answers questionNumber={2} left = 'アクセス' right='営業形態'/>
+      <h2 className={styles.question}>←営業形態&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;価格→</h2>
+      <Answers questionNumber={3} left='営業形態' right='価格'/>
       <DiagnoseButton />
     </div>
   )
